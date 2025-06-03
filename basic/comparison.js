1c1a725b96e98dc61fa0b68c9e4a95a1af2d954b
CodeBender <157215759+codeBender007@users.noreply.github.com>
2025-06-03 06:45:01 +0000

    string file

--- basic/comparison.js
+++ basic/comparison.js
@@ -1,13 +1,14 @@
-// console.log("2" > 2)
-// console.log("02" > 2)
+console.log("2" > 2)
+console.log("02" > 2)
 
-// console.log(null > 0)
-// console.log(null == 0)
-// console.log(null >= 0)
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
 
-// console.log(undefined == 0)
-// console.log(undefined > 0)
-// console.log(undefined < 0)
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
 
 console.log("4" == 2)
+
 
---
+++ basic/string.js
@@ -0,0 +1,9 @@
+
+let str = " adnan pasha "
+// console.log(str.trim())
+
+let str2 = "httpl://adnan.com/page2"
+let str3 = "adnan-pasha-from-mewar"
+console.log(str3.split("-"))
+
+console.log(str3.replace("-"," "))
