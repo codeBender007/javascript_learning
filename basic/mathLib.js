128e5934119ed02c7c047312ccd87c573889dca7
CodeBender <157215759+codeBender007@users.noreply.github.com>
2025-06-03 07:29:15 +0000

    math library

---
+++ basic/mathLib.js
@@ -0,0 +1,16 @@
+let num1 = 34
+console.log(num1)
+console.log(num1.toString().length)
+
+
+// Math library
+let num = 34.61768
+console.log(Math.round(num))
+console.log(`this is min value ${Math.min(12,2,3,4,5,6)}`)
+console.log(`this is max value ${Math.max(12,2,3,4,5,6)}`)
+console.log(Math.floor(num))
+
+let num2 = 34.11768
+console.log(Math.ceil(num))
+
+console.log(Math.floor(Math.random()*10)+1)
