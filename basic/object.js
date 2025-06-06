d5fb4a85548ced8a5f97143d643556b6c639a49e
CodeBender <157215759+codeBender007@users.noreply.github.com>
2025-06-06 06:33:46 +0000

    object

---
+++ basic/object.js
@@ -0,0 +1,14 @@
+// 1) using constructor 
+
+// 2) object literal 
+
+const obj1 = {
+    name:"adnan",
+    roll:123
+}
+const obj2 = {
+    name:"ayan",
+    roll:234
+}
+
+const obj3 = Object.assign(obj1 , obj2)
