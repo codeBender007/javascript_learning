14e41b7409165e816b443e65dc9947c6c2989f3f
CodeBender <157215759+codeBender007@users.noreply.github.com>
2025-06-06 06:44:27 +0000

    some code

--- basic/object.js
+++ basic/object.js
@@ -3,12 +3,20 @@
 // 2) object literal 
 
 const obj1 = {
-    name:"adnan",
-    roll:123
+    name1:"adnan",
+    roll1:123
 }
 const obj2 = {
-    name:"ayan",
-    roll:234
+    name2:"ayan",
+    roll2:234
 }
 
-const obj3 = Object.assign(obj1 , obj2)
+// accessing : agr multiple objects ko merge krna h to keys different honi chahiya value agr same ho to koi frk ni pdta
+const obj3 = Object.assign({},obj1 , obj2)
+// const obj3 = {...obj1 , ...obj2}
+
+console.log(obj3)
+
+// in methods se ham object ki sari keys or values dekh skte h 
+console.log(Object.keys(obj3))
+console.log(Object.values(obj3))
