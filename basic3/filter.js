6c5b10bc2414c4fad3ddf6f8098923a22708fb82
CodeBender <157215759+codeBender007@users.noreply.github.com>
2025-06-19 06:45:33 +0000

    filter

---
+++ basic3/filter.js
@@ -0,0 +1,9 @@
+// agr curlibracket lga diya to return keyword use krna hi hoga return ke liya 
+// or agr curlibracket ni lgay to return keyword ka use krne ki koi need ni 
+// or filter bi forEach ki trh sirf array pr hi use hota h qki ya dono array ke hi method hai
+
+
+const arr = [9,8,7,6,5,4,3,2,1]
+
+const or = arr.filter( (val) => {return val<4} )
+console.log(or)
